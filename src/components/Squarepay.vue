f7995adf0706dbaf8393e3ac80190159af4d0ab0
Chapman <dale.chapman@ensembleaustralia.com.au>
2017-03-21 17:13:23 +1100

    finished component ready to package

--- src/components/Squarepay.vue
+++ src/components/Squarepay.vue
@@ -1,34 +1,73 @@
 
 <script>
 export default {
-    data: function() {
+    data() {
         return {
-            name: "PAY ME"
+            name: "TRANSACTION"
         }
     },
     computed : {
         href() {
-            return `intent:#Intent;action=com.squareup.register.action.CHARGE;package=com.squareup;S.browser_fallback_url=https://square.stage.ensemble.agency;S.com.squareup.register.WEB_CALLBACK_URI=https://square.stage.ensemble.agency;S.com.squareup.register.CLIENT_ID=${ this.client };S.com.squareup.register.API_VERSION=v1.3;i.com.squareup.register.TOTAL_AMOUNT=${ this.amount };S.com.squareup.register.CURRENCY_CODE=${ this.currency };S.com.squareup.register.TENDER_TYPES=com.squareup.register.TENDER_CARD,com.squareup.register.TENDER_CARD_ON_FILE,com.squareup.register.TENDER_CASH,com.squareup.register.TENDER_OTHER;end`
+            // return `intent:#Intent;action=com.squareup.register.action.CHARGE;package=com.squareup;S.browser_fallback_url=https://square.stage.ensemble.agency;S.com.squareup.register.WEB_CALLBACK_URI=https://square.stage.ensemble.agency;S.com.squareup.register.CLIENT_ID=${ this.client };S.com.squareup.register.API_VERSION=v1.3;i.com.squareup.register.TOTAL_AMOUNT=${ this.amount };S.com.squareup.register.CURRENCY_CODE=${ this.currency };S.com.squareup.register.TENDER_TYPES=com.squareup.register.TENDER_CARD,com.squareup.register.TENDER_CARD_ON_FILE,com.squareup.register.TENDER_CASH,com.squareup.register.TENDER_OTHER;end`
+            const data = {
+                'action': 'com.squareup.register.action.CHARGE',
+                'package': 'com.squareup',
+                'S.browser_fallback_url': this.callbackUrl,
+                'S.com.squareup.register.WEB_CALLBACK_URI': this.callbackUrl,
+                'S.com.squareup.register.CLIENT_ID': this.client,
+                'S.com.squareup.register.API_VERSION': 'v1.3',
+                'i.com.squareup.register.TOTAL_AMOUNT': this.amount,
+                'S.com.squareup.register.CURRENCY_CODE': this.currency,
+                'S.com.squareup.register.TENDER_TYPES': [
+                    'com.squareup.register.TENDER_CARD',
+                    'com.squareup.register.TENDER_CARD_ON_FILE',
+                    'com.squareup.register.TENDER_CASH',
+                    'com.squareup.register.TENDER_OTHER'
+                ]
+            };
+
+            let href = '';
+
+            for (let prop in data) {
+            	href += prop + '=';
+
+            	if (Array.isArray(data[prop])) href += data[prop].join(',');
+              else href += data[prop];
+
+              href += ';'
+            }
+            console.log("intent:#Intent;" + href + "end");
+            return "intent:#Intent;" + href + "end";
         }
     },
     props: {
-        client: 1234567890,
-        currency: "AUD",
-        amount: 0 // amount for transaction
+        client: {
+            type: [String, Number],
+            default: 1234567890
+        }, // CLIENT_ID.
+        currency: {
+            type: String,
+            default: 'AUD'
+        }, // Currency to use in transaction.
+        amount: {
+            type: Number,
+            default:  0
+        }, // Amount in cents eg. 100 = $1.
+        callbackUrl: {
+            type: String,
+            default: 'https://square.stage.ensemble.agency'
+        }, // Fallback URL
     },
     methods : {
-        txnSent() {
-            alert('the transaction was sent');
-        }
     }
 }
 </script>
 
 <template>
     <div class="button-wrapper">
-        <a :client="client" :currency="currency" :amount="amount" :href="href"><button @click="txnSent()" type="button" name="button">{{ name }}</button></a>
+        <a class="square-pay-button" :href="href"><slot></slot></a>
 
-        <!-- html for results -->
+        <!-- html for result wip -->
         <!-- <transition name="fade">
             <div v-if="amount > 0" class="results">
                  Paid {{ amount }} {{ currency }} to {{ client }}
@@ -40,18 +79,27 @@ export default {
 <style lang="sass" scoped>
     .button-wrapper {
         position: relative;
-        border: 2px solid black;
+        border: 1px solid #ccc;
         min-height: 150px;
+        min-width: 250px;
         width: 100%;
-        button {
-            padding: 8px 25px;
+        .square-pay-button {
+            text-decoration: none;
+            padding: 10px 25px;
             color: #333;
-            border: 2px solid #333;
+            border: 1px solid #444;
             border-radius: 3px;
             position: absolute;
             top: 50%;
             left: 50%;
+            transition: all .2s ease-in-out;
             transform: translate(-50%, -50%);
+            &:hover {
+                background: #555;
+                color: #f9f9f9;
+                border: 1px solid transparent;
+                filter: drop-shadow(0px 2px 1px rgba(0,0,0,.4));
+            }
         }
         .fade-enter-active, .fade-leave-active {
             transition: opacity 0.8s;
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import store from './store';
-import welcome from '../components/welcome.vue';
 import { modal, checkbox } from 'aurora-ui';
 import Squarepay from '../components/Squarepay.vue';
 
@@ -10,5 +9,8 @@ Vue.use(VueResource);
 new Vue({
 	el: '#app',
 	store,
-	components: { checkbox, modal, welcome, Squarepay }
+	components: { checkbox, modal, Squarepay },
+	data: {
+		name : " BUTTON"
+	}
 });
